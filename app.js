fa153bd7d19b1e67db65172c4d2a4187ca0bcba0
ldsimonassi2 <97476049+ldsimonassi2@users.noreply.github.com>
2024-12-17 01:47:32 -0300

    reduce smell by 1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const awsLambdaFastify = require('@fastify/aws-lambda');
 const server = require('./server');
 
-const proxy = awsLambdaFastify(server)
+const proxy = awsLambdaFastify(server);
 
 exports.handler = proxy;
